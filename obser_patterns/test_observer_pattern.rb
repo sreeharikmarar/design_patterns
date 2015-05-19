639785059271fcc02801bb532615757d6510809b
sreeharikmarar <sreeharikmarar@gmail.com>
2015-05-19 11:57:44 +0530

    modifications

--- obser_patterns/test_observer_pattern.rb
+++ obser_patterns/test_observer_pattern.rb
@@ -6,11 +6,11 @@ require "test/unit"
 class TestObseverPattern < Test::Unit::TestCase
  
   def test_simple
-     company_1 = Company.new("abc",50)
-	   company_2 = Company.new("abc",60)
+    company_1 = Company.new("abc",50)
+	  company_2 = Company.new("abc",60)
 
-	   employee_1 = Employee.new("sam")
-	   employee_2 = Employee.new("tom")
+		employee_1 = Employee.new("sam")
+		employee_2 = Employee.new("tom")
 
 	
     assert_equal([employee_1], company_1.add_observer(employee_1) )
