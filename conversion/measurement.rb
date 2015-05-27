b6252ff17d85d8deb7b5d8d190756570cbaa564f
sreeharikmarar <sreeharikmarar@gmail.com>
2015-05-27 09:54:00 +0530

    conversion problem

---
+++ conversion/measurement.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'pry'
+
+class Measurement 
+	
+	attr_accessor :amount
+
+	def initialize(amount)
+     @amount = Integer(amount)
+  end
+
+	def cm
+		self
+	end
+
+	def *(factor)
+    return Measurement.new(self.amount * factor.amount)
+  end
+
+  def /(factor)
+    return Measurement.new(self.amount / factor.amount)
+  end
+
+  def to_f
+  	return amount.to_f
+  end
+
+	def to_mm
+		mm = cm * Measurement.new(10)
+		mm.amount
+	end
+
+	def to_m
+		cm.amount.to_f / Measurement.new(100).to_f
+	end
+
+end
---
+++ conversion/test_measurement.rb
@@ -0,0 +1,14 @@
+require_relative "measurement"
+
+require "test/unit"
+
+class TestMeasurement < Test::Unit::TestCase
+
+  def test_cm_conversion
+    
+    assert_equal(Measurement.new(1).cm.to_mm,10)
+    assert_equal(Measurement.new(1).cm.to_m,0.01)
+
+  end
+
+end
