e9d5e7fbfac7c0239334234686fc481bc572f97d
sreeharikmarar <sreeharikmarar@gmail.com>
2015-05-27 09:59:10 +0530

    conversion problem changes

--- conversion/measurement.rb
+++ conversion/measurement.rb
@@ -2,18 +2,18 @@ require 'rubygems'
 require 'pry'
 
 class Measurement 
-	
-	attr_accessor :amount
 
-	def initialize(amount)
-     @amount = Integer(amount)
+  attr_accessor :amount
+
+  def initialize(amount)
+    @amount = Integer(amount)
   end
 
-	def cm
-		self
-	end
+  def cm
+    self
+  end
 
-	def *(factor)
+  def *(factor)
     return Measurement.new(self.amount * factor.amount)
   end
 
@@ -22,16 +22,16 @@ class Measurement
   end
 
   def to_f
-  	return amount.to_f
+    return amount.to_f
   end
 
-	def to_mm
-		mm = cm * Measurement.new(10)
-		mm.amount
-	end
+  def to_mm
+    mm = cm * Measurement.new(10)
+    mm.amount
+  end
 
-	def to_m
-		cm.amount.to_f / Measurement.new(100).to_f
-	end
+  def to_m
+    cm.amount.to_f / Measurement.new(100).to_f
+  end
 
 end
