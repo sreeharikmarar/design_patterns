6b55a10835223f96aa75ba83d162a88ee3155bee
sreeharikmarar <sreeharikmarar@gmail.com>
2015-05-27 09:56:54 +0530

    conversion problem test updated

--- conversion/test_measurement.rb
+++ conversion/test_measurement.rb
@@ -7,7 +7,10 @@ class TestMeasurement < Test::Unit::TestCase
   def test_cm_conversion
     
     assert_equal(Measurement.new(1).cm.to_mm,10)
+    assert_equal(Measurement.new(2).cm.to_mm,20)
+
     assert_equal(Measurement.new(1).cm.to_m,0.01)
+    assert_equal(Measurement.new(2).cm.to_m,0.02)
 
   end
 
