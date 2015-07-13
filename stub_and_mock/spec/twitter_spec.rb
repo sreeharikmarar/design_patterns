72eaf56dac5b5d363f667fb3e7c5f68654668b29
sreeharikmarar <sreeharikmarar@gmail.com>
2015-07-13 11:30:54 +0530

    test are updated

--- stub_and_mock/spec/twitter_spec.rb
+++ stub_and_mock/spec/twitter_spec.rb
@@ -21,16 +21,32 @@ describe "Twitter object stub" do
   end
 end
 
-# describe "Twitter verify Post method" do
+describe "Twitter verify Post method" do
 
-#   it "verify post method being called or not" do
-#     allow_any_instance_of(Twitter).to receive(:post).with("sample tweet").and_return(true)
-#     allow_any_instance_of(TwitterController).to receive(:post).with("sample tweet").and_return("sample post")
+  it "verify post method being called or not" do
+    allow_any_instance_of(Twitter).to receive(:post).with("sample tweet").and_return(true)
+    allow_any_instance_of(TwitterController).to receive(:post).with("sample tweet").and_return("sample tweet")
     
-#     twitter = Twitter.new
-#     controller = TwitterController.new(twitter)
+    twitter = Twitter.new
+    controller = TwitterController.new(twitter)
 
-#     expect(twitter.post("sample tweet")).to eq(true)
-#     expect(controller.post("sample tweet")).to eq("sample tweet")
-#   end
-# end
+    expect(twitter.post("sample tweet")).to eq(true)
+    expect(controller.post("sample tweet")).to eq("sample tweet")
+  end
+end
+
+
+describe "Twitter controller verify Post method" do
+   it "verify post method being called or not" do
+      twitter = spy('twitter')
+      twitter_controller = spy('twitter_controller')
+      
+      twitter = twitter.new
+      twitter.post("new post")
+      twitter_controller = twitter_controller.new(twitter)
+      twitter_controller.post("new post")
+      
+      expect(twitter).to have_received(:post).with("new post")
+      expect(twitter_controller).to have_received(:post).with("new post")
+  end
+end
